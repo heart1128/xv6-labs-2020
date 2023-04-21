5eb813ff9be3f783effbb5a943e2b663e882a68c
reedleaf <1020273485@qq.com>
2023-04-21 09:13:09 +0800

    fs.asm

--- push.sh
+++ push.sh
@@ -1,12 +1,11 @@
 #!/bin/zsh
 
+# $0是文件名
 # 添加文件，第一个参数是添加的文件
-git add $0
-
-echo $1
+git add $1
 
 # 提交。第二个参数是注释
-git commit -m $1
+git commit -m $2
 
 # 上传，第三个参数是分支
-git push -u origin $2
+git push -u origin $3
